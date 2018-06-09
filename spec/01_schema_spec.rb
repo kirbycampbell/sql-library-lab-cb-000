af1d335f05886c202d4f7328ed83d5fc923002d8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 23:49:33 +0000

    automatically backed up by learn

--- spec/01_schema_spec.rb
+++ spec/01_schema_spec.rb
@@ -2,7 +2,7 @@ describe 'creating schema' do
   before do
     @db = SQLite3::Database.new(':memory:')
     @sql_runner = SQLRunner.new(@db)
-    @sql_runner.execute_schema_sql
+    @sql_runner.execute_schema_sql()
   end
 
   describe 'series table' do
